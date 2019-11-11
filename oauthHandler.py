14e3f65eb91ac8be21745e4ee1e88d9adeae5179
Mani bharathy <manibharathytu5194@gmail.com>
2019-11-11 13:33:42 +0530

    no cred in public repo

--- oauthHandler.py
+++ oauthHandler.py
@@ -8,14 +8,14 @@ import tweepy
 
 # The consumer keys can be found on your application's Details
 # page located at https://dev.twitter.com/apps (under "OAuth settings")
-consumer_key="xqIOKUMXfvzvCckqfxIqnMrYk"
-consumer_secret="YfjHXWNek98tv4WHXWpaHKJsLQVkXE5KYQDPZyPk8qRbGfIkz8"
+consumer_key=""
+consumer_secret=""
 
 # The access tokens can be found on your applications's Details
 # page located at https://dev.twitter.com/apps (located 
 # under "Your access token")
-access_token="1193427579035766784-AKi5gMOrDn01mBq8i76qHzNzVKzPsw"
-access_token_secret="DqzFO5vCWqgZKDBLdZFi14NtjlXjo6OJJrE1LRaOsDsWP"
+access_token=""
+access_token_secret=""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
