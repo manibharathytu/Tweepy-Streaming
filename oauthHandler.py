73d9dfd313752a5b0c7b1f6c25fa44a6af85f42c
Mani bharathy <manibharathytu5194@gmail.com>
2019-11-11 13:10:10 +0530

    streaming done

--- oauthHandler.py
+++ oauthHandler.py
@@ -8,14 +8,14 @@ import tweepy
 
 # The consumer keys can be found on your application's Details
 # page located at https://dev.twitter.com/apps (under "OAuth settings")
-consumer_key="CONSUMER KEY HERE"
-consumer_secret="CONSUMER SECRET HERE"
+consumer_key="xqIOKUMXfvzvCckqfxIqnMrYk"
+consumer_secret="YfjHXWNek98tv4WHXWpaHKJsLQVkXE5KYQDPZyPk8qRbGfIkz8"
 
 # The access tokens can be found on your applications's Details
 # page located at https://dev.twitter.com/apps (located 
 # under "Your access token")
-access_token="ACCESS TOKEN HERE"
-access_token_secret="ACCESS TOKEN SECRET HERE"
+access_token="1193427579035766784-AKi5gMOrDn01mBq8i76qHzNzVKzPsw"
+access_token_secret="DqzFO5vCWqgZKDBLdZFi14NtjlXjo6OJJrE1LRaOsDsWP"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -23,7 +23,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 def getAPI():
 	api = tweepy.API(auth)
-	print "DEBUG: " + api.me().name + " is no logged in!"
+	print ("DEBUG: " + api.me().name + " is no logged in!")
 	return api
 
 def getAuth():
--- streaming.py
+++ streaming.py
@@ -10,35 +10,6 @@ import json
 tweetDict = dict() 
 parsedTweets = 0
 
-#Handeling tweets, updating dict, and writing a log
-def parseTweet(tweet):
-	global tweetDict
-	global parsedTweets
-	#Look for all tags and update our tweetDict
-	for tag in tweet.hashtags:
-		t = tag["text"]
-		if(tweetDict.has_key(t)):
-			tweetDict[t] += 1
-		else:
-			tweetDict[t] = 1
-	parsedTweets += 1
-	#Every 10th tweet we will output the most popular tag and write the info to the log
-	if(parsedTweets > 10):
-		parsedTweets = 0
-		maxi = 0
-		maxTag = str()
-		count = 0
-		for tag in tweetDict:
-			num = tweetDict[tag]
-			count += num
-			if(num > maxi and tag.lower() != "android"):
-				maxi = num
-				maxTag = tag
-		output = "Most Popular: " + maxTag + " with " + str(maxi) + ". Dict Entries: " + str(len(tweetDict)) + ". Tags parsed: " + str(count)
-		f = open("log.txt","a")
-		f.write(output + "\n")
-		print output 
-			
 
 #Tweet class with all the information we need for this program (Hashtags and the actual tweet text)
 class Tweet:
@@ -47,6 +18,7 @@ class Tweet:
 
         def __init__(self, json):
 		self.text = json["text"] 
+		self.username = json["user"]["screen_name"]
 		self.hashtags = json["entities"]["hashtags"]
 
 #Basic listener which parses the json, creates a tweet, and sends it to parseTweet
@@ -54,11 +26,12 @@ class TweetListener(StreamListener):
 	def on_data(self, data):
 		jsonData = json.loads(data)
 		tweet = Tweet(jsonData)
-		parseTweet(tweet)
+		print(tweet.text.encode('utf-8'))
+		print(tweet.username.encode('utf-8'))
 		return True
 
 	def on_error(self, status):
-		print status
+		print (status)
 
 
 if __name__ == '__main__':
@@ -66,6 +39,7 @@ if __name__ == '__main__':
 	listener = TweetListener()
 	auth = oauthHandler.getAuth()
 	stream = Stream(auth, listener)	
-	stream.filter(track=['android']) #This will start the stream and make callbacks to the listener for all tweets containing "android"
+	filt = ['yanggang']
+	stream.filter(track=filt) #This will start the stream and make callbacks to the listener for all tweets containing "android"
 
 
